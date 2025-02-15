7d6440b1be2e55c9c3103ab17a57ab094079b1a8
hirariin <kanako.hirabayashi04@gmail.com>
2025-02-16 01:48:52 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
    belongs_to :user
    has_one_attached :image
+
+   extend ActiveHash::Associations::ActiveRecordExtensions
+   belongs_to :item_category
+   belongs_to :item_sales_status
+   belongs_to :item_shipping_fee_status
+   belongs_to :prefecture
+   belongs_to :item_scheduled_delivery
+
+   validates :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id,
+             :prefecture_id, :item_scheduled_delivery_id, :sell_price, presence: true
+
+   validates :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :prefecture_id,
+             :item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
