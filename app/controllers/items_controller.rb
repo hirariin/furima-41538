cb9edab9a7b43d04b21a753867cb8f7c6b73bdce
hirariin <kanako.hirabayashi04@gmail.com>
2025-02-18 15:54:20 +0900

    エラーメッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,9 +29,8 @@ class ItemsController < ApplicationController
   end
   
   def update
-    item =Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to root_path
+    if @item.update(item_params)
+       redirect_to @item
     else
       render :edit, status: :unprocessable_entity
   end
