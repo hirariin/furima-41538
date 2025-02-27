ae87480aef2c18001709ed67baccb3ca2d88709f
hirariin <kanako.hirabayashi04@gmail.com>
2025-02-27 22:14:03 +0900

    テストコードの実装

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :shipping_address do
+  factory :order_form do
+    user_id { 1 }
+    item_id { 1 }
     postal_code { '123-4567' }
     prefecture_id { 2 }
     city { '横浜市' }
---
+++ spec/models/order_form_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe OrderForm, type: :model do
+
+  before do
+    user = FactoryBot.create(:user)
+    @order_form = FactoryBot.build(:order_form, user_id: user.id)
+  end
+  describe '配属先情報の保存' do
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_form).to be_valid
+      end
+      it 'buildingは空でも保存できること' do
+        @order_form.building = ''
+        expect(@order_form).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @order_form.postal_code = ''
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @order_form.postal_code = '1234567'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+      end
+      it 'prefectureを選択していないと保存できないこと' do
+        @order_form.prefecture_id = 0
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'cityが空だと保存できないこと' do
+        @order_form.city = ''
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("City can't be blank")
+      end
+      it 'addressesが空だと保存できないこと' do
+        @order_form.addresses = ''
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Addresses can't be blank")
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @order_form.phone_number = ''
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが10桁以上11桁以内の半角数字でない場合、保存できないこと' do
+        @order_form.phone_number = '090123456'
+        expect(@order_form).not_to be_valid
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid. Must be 10 to 11 digits long and contain only numbers')
+        
+        @order_form.phone_number = '090123456789'
+        expect(@order_form).not_to be_valid
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid. Must be 10 to 11 digits long and contain only numbers')
+        
+        @order_form.phone_number = '090-1234-5678'
+        expect(@order_form).not_to be_valid
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid. Must be 10 to 11 digits long and contain only numbers')
+        
+        @order_form.phone_number = '090abcd5678'
+        expect(@order_form).not_to be_valid
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid. Must be 10 to 11 digits long and contain only numbers')
+      end
+      
+    end
+  end
+end
