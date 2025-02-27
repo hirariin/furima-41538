62bc1a387aa994a5955e7a2d4af9d3403da6bc33
hirariin <kanako.hirabayashi04@gmail.com>
2025-02-27 14:37:15 +0900

    データをテーブルに保存

--- app/models/order_shippingaddress.rb
+++ app/models/order_shippingaddress.rb
@@ -13,4 +13,8 @@ class OrderShippingAddress
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAddress(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number )
+  end
 end
