1a0abc3dc6baddb781b2431a45659793a24a6d92
hirariin <kanako.hirabayashi04@gmail.com>
2025-02-27 18:51:42 +0900

    エラー表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,28 @@
 class OrdersController < ApplicationController
+before_action :set_item, only: [:index, :create]
 
 def index
   @order_form = OrderForm.new
 end
 
+
+def create
+  @order_form = OrderForm.new(order_form_params)
+  if @order_form.valid?
+    @order_form.save
+    redirect_to root_path
+  else
+    render :index, status: :unprocessable_entity
+  end
+end
+
+private
+
+def set_item
+  @item = Item.find(params[:item_id])
+end
+
+def order_form_params
+  params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,11 +1,11 @@
-class OrderShippingAddress
-  include ActiveModel :: Model
+class OrderForm
+  include ActiveModel::Model
   attr_accessor :user_id, :item_id, :order_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :order_id
+    #validates :order_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :addresses
@@ -15,6 +15,6 @@ class OrderShippingAddress
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    ShippingAddress(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number )
+    ShippingAddress.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number )
   end
 end
